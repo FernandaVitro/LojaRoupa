5a419b5a416576cf6e53446d7580f4a571c0974c
lauhelo <clevestonlaura@gmail.com>
2026-05-26 16:03:37 -0300

    Add files via upload

---
+++ laura/Sistema_MVC/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const pessoaRoutes = require('./scr/routes/pessoasRoutes')
+const vendasRoutes = require('./scr/routes/vendasRoutes');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use(pessoaRoutes);
+app.use(vendasRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=> console.log(`server em http://localhost:${PORT}`))
+
