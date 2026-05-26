c014e51edd0e98e91582b2299a1388f7f9cf1e39
lauhelo <clevestonlaura@gmail.com>
2026-05-26 17:12:36 -0300

    Add files via upload

--- SistemaMVC/src/routes/pessoasRoutes.js
+++ SistemaMVC/src/routes/pessoasRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const pessoasController = require('../controllers/pessoasControllers');
+const pessoaController = require('../controller/pessoasController');
 
-
-router.get('/pessoas', pessoasController.index);
-
-router.delete('/pessoas/:id', pessoasController.delete);
-
-router.post('/pessoas', pessoasController.store);
-
-router.put('/pessoas/:id', pessoasController.update);
-
-
+router.get('/pessoas', pessoaController.index);
+router.post('/pessoas', pessoaController.store);
+router.put('/pessoas/:id', pessoaController.update);
+router.delete('/pessoas/:id', pessoaController.delete);
 
 module.exports = router;
